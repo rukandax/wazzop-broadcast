111287e33940181f3a434d176c2c35bc407d0f61
Rukanda Faridsi <rukanda.saeful@krom.id>
2024-11-13 12:23:48 +0700

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -477,7 +477,7 @@ export default function Home() {
 
       setBroadcastFormData((prevData) => ({
         ...prevData,
-        groupId: "",
+        groups: [],
       }));
     } catch (error: any) {
       toast({
